2fd84b6f7d80127e07505ac0bc6d41fea5621cca
Aokijikuzan <florenzabs@hotmail.fr>
2020-04-09 19:02:55 +0200

    vif du sujet

---
+++ ng-pokemons-app/src/app/app.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core'; 
+  
+@Component({ 
+ selector: 'app-root', 
+ template: `<h1>Hello, Angular 2 !</h1>`, 
+})
+export class AppComponent implements OnInit{ 
+ private pokemons : Pokemon[];
+ 
+ ngOnInit()
+ {
+   this.pokemons=[
+     { name: 'Bulbizarre', hp: 6},
+     { name: 'Salamèche', hp: 6},
+     { name: 'Carapuce', hp: 6}
+   ];
+ }
+}
