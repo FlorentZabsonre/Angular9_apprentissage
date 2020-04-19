ed4ee779d5b3ce07dca275c8860afe129b915abd
Aokijikuzan <florenzabs@hotmail.fr>
2020-04-19 18:08:38 +0200

    grid pokemon

--- ng-pokemons-app/src/app/app.module.ts
+++ ng-pokemons-app/src/app/app.module.ts
@@ -2,11 +2,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-
+import { BorderCardDirective } from './border-card.directive';
 @NgModule({
   declarations: [
     AppComponent,
-  
+    BorderCardDirective 
   ],
   imports: [
     BrowserModule
